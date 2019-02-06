440fedb9736d8482679c81b12f378fb80b3eaf21
shivachit1 <shivat@metropolia.fi>
2019-02-06 21:53:26 +0200

    Commit with SignUp Dialog Fragment

--- app/src/main/java/com/example/ours/LogInActivity.java
+++ app/src/main/java/com/example/ours/LogInActivity.java
@@ -1,7 +1,11 @@
 package com.example.ours;
 
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 public class LogInActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class LogInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+
+        TextView signuphere=findViewById(R.id.signuphere);
+
+        signuphere.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showSignupDailog();
+            }
+        });
+    }
+
+    private void showSignupDailog() {
+
+        DialogFragment signupDailogfragment=new DialogFragment();
+        
     }
 }
---
+++ app/src/main/java/com/example/ours/dialogfragments/signUpFragment.java
@@ -0,0 +1,31 @@
+package com.example.ours.dialogfragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.ours.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class signUpFragment extends DialogFragment {
+
+
+    public signUpFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sign_up, container, false);
+    }
+
+}
